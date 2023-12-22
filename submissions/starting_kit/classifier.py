6b4582c22c3065a8d207286ae298658122619fb8
AntoineTSP <antoine.klein@telecom-sudparis.eu>
2023-12-22 14:12:57 +0100

    V3: submissions template

---
+++ submissions/starting_kit/classifier.py
@@ -0,0 +1,95 @@
+from sklearn.base import BaseEstimator
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.impute import OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+
+description_features_cat = ["type_sol",
+                            'classe_age',
+                            'classe_hauteur',
+                            'classe_circonference',
+                            'vigueur_pousse',
+                            'plaie_collet',
+                            'fissure_tronc',
+                            'plaie_tronc',
+                            'fissure_houppier',
+                            'bois_mort_houppier',
+                            'plaie_houppier',
+                            'prescription_1',
+                            'prescription_2',
+                            'prescription_3',
+                            'type_delai_1',
+                            'type_delai_2']
+description_features_string = ["ID_ARBRE",
+                               'commune',
+                               'quartier',
+                               'site',
+                               'cote_voirie',
+                               'genre_arbre',
+                               'espece_arbre',
+                               'situation',
+                               'port_arbre',
+                               'champignon_collet',
+                               'insecte_collet',
+                               'observation_collet',
+                               'champignon_tronc',
+                               'insecte_tronc',
+                               'rejet_tronc',
+                               'tuteurage_arbre',
+                               'canisse_arbre',
+                               'observation_tronc',
+                               'champignon_houppier',
+                               'insecte_houppier',
+                               'ecorce_incluse_houppier',
+                               'observation_houppier',
+                               'contrainte',
+                               'date_diagnostic',
+                               'observation_travaux'
+                               ]
+description_features_num = ['matricule_arbre',
+                            'controle',
+                            'surf_permeable',
+                            'date_plantation',
+                            'hauteur',
+                            'diametre',
+                            'circonference (en cm)',
+                            'esperance_maintien',
+                            'Long',
+                            'Lat'
+                            ]
+target_features = ['classification_diagnostic']
+
+
+class Classifier(BaseEstimator):
+    def __init__(self):
+        self.string_transformer = ColumnTransformer(
+            [("string",
+              OrdinalEncoder(handle_unknown='use_encoded_value',
+                             unknown_value=-1000),
+              description_features_string),
+             ("cat",
+              OrdinalEncoder(handle_unknown='use_encoded_value',
+                             unknown_value=-1000), description_features_cat)])
+
+        self.numeric_transformer = Pipeline(
+            steps=[("imputer", SimpleImputer(strategy="most_frequent")),
+                   ("scaler", StandardScaler())])
+
+        self.model = RandomForestClassifier(random_state=42)
+
+        self.pipe = make_pipeline(
+            self.string_transformer,
+            self.numeric_transformer,
+            self.model)
+
+    def fit(self, X, y):
+        self.pipe.fit(X, y)
+
+    def predict(self, X):
+        return self.pipe.predict(X)
+
+    def predict_proba(self, X):
+        return self.pipe.predict_proba(X)
+    
